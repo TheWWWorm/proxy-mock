f2c5ea5b0df680e5e39bed6afac80faa7d641bcd
Deniss <thewwworm@gmail.com>
2020-06-06 19:44:49 +0300

    initial version

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # proxy-mock
+A mock that can be used for anything
+
+Recursively creates properties on demand(they are created as new copy of proxy-mock), can be called as function, supports toPrimitive and iterator methods.
+
+## Use
+
+```js
+const mock = getProxyMock();
+const { a, b, c} = mock; // a, b, c will be new proxy-mock copies
+const nested = mock.db.users[3].address.street; // will work, all the keys will be created as new proxy-mock copies
+const fnResult = mock.get('/cats'); // will result in yet another proxy-mock copy
+const math = mock * 2; // will result in 84, since in case of toPrimitive calls we return primitive value, in this case it's 42
+```
+
+### auto spy(jasmine example)
+proxy mock supports auto spying on newly created properties, you just have to pass the the spy function to proxy-mock
+```js
+const spyFn = (name, fn) => {
+    return jasmine.createSpy(name, fn).and.callThrough();
+}
+const mock = getProxyMock({}, 'httpClient', spyFn);
+mock.post('/api/call');
+expect(mock).toHaveBeenCalled();
+```
+spyFn will be called each time new proxy is created and target will get neatly wrapped by the spy
+
+
+
---
+++ index.js
@@ -0,0 +1,47 @@
+'use strict';
+
+/**
+ * @param {Object} [specifics={}] specifics - allows avoid generating specific keys, will use keys from provided object instead
+ * @param {String} [name='proxyMock'] name - useful in testing scenarios, when you want to see if spy failed or not
+ * @param {Function} spyWrap - spy wrapper, can be used for auto-spying on the proxy. Should accept (name, function) for spying
+ */
+const getProxyMock = (specifics = {}, name = 'proxyMock', wrap) => {
+    function _target() {
+        getProxyMock();
+    }
+
+    const target = wrap ? wrap(name, _target) : _target;
+
+    target[Symbol.toPrimitive] = (hint) => {
+        if (hint === 'string') {
+            return 'proxyString';
+        } else if (hint === 'number') {
+            return 42;
+        }
+        return '1337';
+    }
+    target[Symbol.iterator] = function*() {
+        yield getProxyMock({}, `${name}.${key}`, wrap);
+    }
+
+    return new Proxy(target, {
+        get(obj, key) {
+            key = key.toString();
+            if (specifics.hasOwnProperty(key)) {
+                return specifics[key];
+            }
+            if (key === 'Symbol(Symbol.toPrimitive)') {
+                return obj[Symbol.toPrimitive]
+            }
+            if (key === 'Symbol(Symbol.iterator)') {
+                return obj[Symbol.iterator]
+            }
+            if (!obj.hasOwnProperty(key)) {
+                obj[key] = getProxyMock({}, `${name}.${key}`, wrap);
+            }
+            return obj[key];
+        }
+    })
+}
+
+exports.getProxyMock = getProxyMock;
